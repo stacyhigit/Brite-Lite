777262eea22eef405e5e37e29c25675d02535ed1
stacyhigit <64869133+stacyhigit@users.noreply.github.com>
2024-04-26 18:23:52 -0400

    remove android/app/.cxx

--- android/app/.cxx/Debug/y2u426k6/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "C:/Stacy/react-native/projects/publish/Brite-Lite/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/374i1y3l/obj/arm64-v8a/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Stacy/react-native/projects/publish/Brite-Lite/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/y2u426k6/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Stacy/.gradle/caches/transforms-3/e2c6288cc5e8b3ecc9940685df93e0cc/transformed/jetified-hermes-android-0.73.6-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Stacy/.gradle/caches/transforms-3/e2c6288cc5e8b3ecc9940685df93e0cc/transformed/jetified-hermes-android-0.73.6-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
