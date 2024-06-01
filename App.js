66186427bb375307582945878a66d68c9830cf8c
stacyhigit <64869133+stacyhigit@users.noreply.github.com>
2024-06-01 13:59:59 -0400

    refactor toasts

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
 import Main from "./app/components/Main";
 import OpenScreen from "./app/components/footer/OpenScreen";
 import BoardContextProvider from "./app/store/board-context";
+import { buttonColors } from "./app/constants/colors";
 
 const Stack = createNativeStackNavigator();
 const navTheme = {
@@ -11,6 +16,35 @@ const navTheme = {
     ...DefaultTheme.colors,
     background: "transparent",
   },
+};
+
+const toastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      contentContainerStyle={styles.toastContainer}
+      style={styles.success}
+      text1Style={styles.text1Style}
+      renderLeadingIcon={() => (
+        <View style={styles.icon}>
+          <MaterialCommunityIcons name="check-circle" size={28} color="white" />
+        </View>
+      )}
+    />
+  ),
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      contentContainerStyle={styles.toastContainer}
+      style={styles.error}
+      text1Style={styles.text1Style}
+      renderLeadingIcon={() => (
+        <View style={styles.icon}>
+          <MaterialCommunityIcons name="alert-circle" size={28} color="white" />
+        </View>
+      )}
+    />
+  ),
 };
 
 export default function App() {
@@ -31,7 +65,31 @@ export default function App() {
             options={{ title: "Open" }}
           />
         </Stack.Navigator>
+        <Toast config={toastConfig} />
       </NavigationContainer>
     </BoardContextProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  toastContainer: {
+    paddingHorizontal: 12,
+  },
+  text1Style: {
+    color: "white",
+    fontSize: 16,
+  },
+  success: {
+    backgroundColor: buttonColors.green,
+    borderLeftColor: buttonColors.green,
+  },
+  error: {
+    backgroundColor: buttonColors.red,
+    borderLeftColor: buttonColors.red,
+  },
+  icon: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 12,
+  },
+});
